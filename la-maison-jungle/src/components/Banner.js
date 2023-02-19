67bce0df88042d10c52bcedaf9919952a01d539e
Issou83 <issou8@gmail.com>
2023-02-19 09:18:04 +0100

    Crétion du dossier styles pour le CSS

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,6 +1,12 @@
+import '../styles/Banner.css'
+
 function Banner() {
-    const title = "La maison jungle"
-    return (<h1>{title}</h1>)
+  const title = "La maison jungle";
+  return (
+    <div className='lmj-banner'>
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
--- la-maison-jungle/src/components/Cart.js
+++ la-maison-jungle/src/components/Cart.js
@@ -1,16 +1,18 @@
 function Cart() {
-    const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
-    return (<div>
-        <h2>Panier</h2>
-        <ul>
-            <li>Monstera : {monsteraPrice}€</li>
-            <li>Lierre : {ivyPrice}€</li>
-            <li>Fleurs : {flowerPrice}€</li>
-        </ul>
-        <p>Total : {monsteraPrice + ivyPrice + flowerPrice}€</p>
-    </div>)
+  const monsteraPrice = 8;
+  const ivyPrice = 10;
+  const flowerPrice = 15;
+  return (
+    <div>
+      <h2>Panier</h2>
+      <ul>
+        <li>Monstera : {monsteraPrice}€</li>
+        <li>Lierre : {ivyPrice}€</li>
+        <li>Fleurs : {flowerPrice}€</li>
+      </ul>
+      <p>Total : {monsteraPrice + ivyPrice + flowerPrice}€</p>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
