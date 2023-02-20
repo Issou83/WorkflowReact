a3905c29a57157080a6029531aff88a0059d273c
Issou83 <issou8@gmail.com>
2023-02-20 13:12:45 +0100

    Ajout du footer avec rensignement du mail client + stateLocal dans le panier + css de la page maj

--- la-maison-jungle/src/components/PlantItem.js
+++ la-maison-jungle/src/components/PlantItem.js
@@ -1,20 +1,22 @@
-import CareScale from "./CareScale"
+
+import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({id,cover, name, water, light}) {
-    return (
-        <li key={id} className='lmj-plant-item' onClick={() => handldeClick(name)}>
-            <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`}/>
-            <div>
-                <CareScale careType='water' scaleValue={water} />
-                <CareScale careType='light' scaleValue={light} />
-            </div>
-        </li>
-    )
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function handldeClick(plantName) {
-    console.log(`Voulez vous acheter un ${plantName}`);
+function PlantItem({ cover, name, water, light }) {
+	return (
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+			</div>
+		</li>
+	)
 }
 
 export default PlantItem
